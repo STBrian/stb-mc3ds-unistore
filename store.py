5f9de2c3de5ceec3f8746187f8d5abce5fe7deab
STBrian <brichap100@gmail.com>
2024-02-06 15:33:55 -0600

    modified

--- store.py
+++ store.py
@@ -3,6 +3,13 @@ import json
 with open("stb-mc3ds.unistore", "r") as f:
     json_data = json.loads(f.read())
 
+if not ("storeInfo" in json_data):
+    print("storeInfo is missing in file")
+if not ("storeContent" in json_data):
+    print("storeContent is missing in file")
+
+for key in json_data["storeInfo"]:
+    print(key)
+
 element = json_data["storeContent"]
 element_1 = element[0]
-print(element_1["info"])
