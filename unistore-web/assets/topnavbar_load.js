8decd9502c5b6f0f0245f7ff174c8a0a2b3d8260
STBrian <brichap100@gmail.com>
2025-01-22 23:23:43 -0600

    Shared topnavbar

---
+++ unistore-web/assets/topnavbar_load.js
@@ -0,0 +1,6 @@
+fetch('topnavbar.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('topnavbar').innerHTML = html;
+    })
+    .catch(error => console.error("Unable to load topnavbar:", error));
