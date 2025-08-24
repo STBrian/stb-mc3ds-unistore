9f492296f9eacc18e8ab688caa4140a9be8a644e
STBrian <brichap100@gmail.com>
2025-08-24 16:41:44 -0600

    Work to Script Edit View

--- store_gui.py
+++ store_gui.py
@@ -47,6 +47,70 @@ def getCurrentUTCTime() -> str:
     futc_time = f"{futc_time[0]} at {futc_time[1][:-3]} (UTC)"
     return futc_time
 
+class ToolTip:
+    def __init__(self, widget, text, delay=1000):
+        self.widget = widget
+        self.text = text
+        self.delay = delay
+        self.tip_window = None
+        self.after_id = None
+
+        widget.bind("<Enter>", self.schedule_show)
+        widget.bind("<Leave>", self.hide)
+
+    def schedule_show(self, event=None):
+        self.after_id = self.widget.after(self.delay, self.show)
+
+    def show(self):
+        if self.tip_window or not self.text:
+            return
+        
+        x, y, _, cy = self.widget.bbox("insert") or (0,0,0,0)
+        x = x + self.widget.winfo_rootx() + 25
+
+        y = y + cy + self.widget.winfo_rooty() + 25
+
+        self.tip_window = tw = customtkinter.CTkToplevel(self.widget)
+        tw.wm_overrideredirect(True)
+        tw.wm_geometry(f"+{x}+{y}")
+
+        label = customtkinter.CTkLabel(tw, text=self.text, corner_radius=0, fg_color="gray20", text_color="white")
+        label.pack()
+    
+    def hide(self, event=None):
+        if self.after_id:
+            self.widget.after_cancel(self.after_id)
+            self.after_id = None
+            if self.tip_window:
+                self.tip_window.destroy()
+                self.tip_window = None
+
+class ModalWindow(tkinter.Toplevel):
+    def __init__(self, master, title, **kwargs):
+        super().__init__(master, **kwargs)
+        self.master = master
+        self.value = None
+
+        self.title(title)
+        self.center(300, 150)
+        self.resizable(False, False)
+
+        self.transient(master)
+        self.grab_set()
+        self.focus_force()
+
+        self.bind("<FocusOut>", lambda e: self.focus_force())
+    
+    def center(self, width: int, height: int):
+        self.update_idletasks()
+        
+        twidth = self.winfo_screenwidth()
+        theight = self.winfo_screenheight()
+
+        x = (twidth // 2) - (width // 2)
+        y = (theight // 2) - (height // 2)
+        self.geometry(f"{width}x{height}+{x}+{y}")
+
 class StoreContent:
     def __init__(self, path: Path):
         with open(path, "r", encoding="utf-8") as f:
@@ -75,26 +139,230 @@ class StoreContent:
         self.storeInfo: dict = json_content["storeInfo"]
         self.storeContent: list[dict] = json_content["storeContent"]
 
+class SelectNewBlockTypeWindow(ModalWindow):
+    def __init__(self, master, closeCommand=None):
+        super().__init__(master, "Select block type")
+        self.master = master
+        self.closeCommand = closeCommand
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+        mainframe = customtkinter.CTkFrame(self, corner_radius=0)
+        mainframe.grid(column=0, row=0, sticky="wnes")
+
+        btnTypes = ["downloadFile", "downloadRelease", "extractFile", "installCia",
+                    "bootTitle", "mkdir", "rmdir", "move", "copy", "deleteFile",
+                    "promptMessage", "skip", "exit"]
+        for i, element in enumerate(btnTypes):
+            btn = customtkinter.CTkButton(mainframe, text=element, width=150, command= lambda a=element : self.blockTypeSelection(a))
+            btn.grid(column=i % 5, row=i//5, padx=(2, 0), pady=(2, 0))
+        
+        self.center(610, 92)
+
+        if closeCommand != None:
+            self.protocol("WM_DELETE_WINDOW", self.blockTypeSelection)
+
+    def blockTypeSelection(self, type = None):
+        self.value = type
+        self.destroy()
+        if self.closeCommand != None:
+            self.closeCommand()
+
 class ScriptViewFrame(customtkinter.CTkFrame):
     def __init__(self, master, elementData: dict, elementName: str, **kwargs):
         super().__init__(master, **kwargs)
-        self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
 
         self.elementData = elementData
 
+        self.lastElementName = elementName
         self.elementNameVar = customtkinter.StringVar(self, value=elementName)
-        self.elementNameLabel = customtkinter.CTkEntry(self, textvariable=self.elementNameVar)
-        self.elementNameLabel.grid(column=0, row=0, sticky="we")
+        elementNameEntry = customtkinter.CTkEntry(self, textvariable=self.elementNameVar, border_width=0)
+        elementNameEntry.grid(column=0, row=0, sticky="we")
+        elementNameEntry.bind("<FocusOut>", self.checkNameAndUpdate)
 
-        toggleViewBtn = customtkinter.CTkButton(self, text="Toggle View", width=100, fg_color="transparent", corner_radius=0)
-        toggleViewBtn.grid(column=1, row=0)
+        self.toggleViewBtn = tglBtn = customtkinter.CTkButton(self, text="↓", width=50, fg_color="transparent", corner_radius=0, command=self.toggleView)
+        tglBtn.grid(column=1, row=0)
+        ToolTip(tglBtn, "Toggle view")
+
+        self.selectBlockWindow = None
+        self.scriptBlocksFrame = None
+        self.toggle = False
+        self.blocks = []
+
+    def checkNameAndUpdate(self, event=None):
+        if self.lastElementName != self.elementNameVar.get():
+            if self.elementNameVar.get() in self.elementData:
+                suffix = 1
+                while f"{self.elementNameVar.get()}({suffix})" in self.elementData:
+                    suffix += 1
+                self.elementNameVar.set(f"{self.elementNameVar.get()}({suffix})")
+            self.elementData[self.elementNameVar.get()] = self.elementData[self.lastElementName]
+            del self.elementData[self.lastElementName]
+            self.lastElementName = self.elementNameVar.get()
+
+    def createBlocksFrame(self):
+        if self.scriptBlocksFrame == None:
+            self.scriptBlocksFrame = customtkinter.CTkFrame(self)
+            self.scriptBlocksFrame.columnconfigure(0, weight=1)
+            self.scriptBlocksFrame.grid(column=0, row=1, columnspan=2, sticky="wnes", pady=(2, 0))
+
+    def toggleView(self):
+        self.checkNameAndUpdate()
+        if self.toggle:
+            self.toggleViewBtn.configure(text="↓")
+            if self.scriptBlocksFrame != None:
+                self.scriptBlocksFrame.destroy()
+                self.scriptBlocksFrame = None
+        else:
+            self.toggleViewBtn.configure(text="↑")
+            self.createBlocksFrame()
+            self.loadBlocks()
+        self.toggle = not self.toggle
+
+    def reloadBlocks(self):
+        if self.scriptBlocksFrame != None:
+            self.scriptBlocksFrame.destroy()
+            self.scriptBlocksFrame = None
+            self.createBlocksFrame()
+            self.loadBlocks()
+
+    def loadBlocks(self):
+        self.blocks.clear()
+        if self.scriptBlocksFrame != None:
+            for i in range(len(self.elementData[self.lastElementName])):
+                addBlockBtn = customtkinter.CTkButton(self.scriptBlocksFrame, text="+", command=lambda a=i : self.addBlockCallback(a), width=20, height=10, corner_radius=360, fg_color="transparent", border_width=0)
+                addBlockBtn.grid(column=1, row=i*2)
+                ToolTip(addBlockBtn, "Add block")
+                block = ScriptBlock(self.scriptBlocksFrame, self.elementData[self.lastElementName], i, self.reloadBlocks)
+                block.grid(column=0, row=i*2+1, sticky="wnes", padx=(10, 0))
+                self.blocks.append(block)
+            i = len(self.elementData[self.lastElementName])
+            addBlockBtn = customtkinter.CTkButton(self.scriptBlocksFrame, text="+", command=lambda a=i : self.addBlockCallback(a), width=20, height=10, corner_radius=10, fg_color="transparent", border_width=0)
+            addBlockBtn.grid(column=1, row=i*2)
+            ToolTip(addBlockBtn, "Add block")
+
+    def addBlockCallback(self, idx: int):
+        if self.selectBlockWindow == None:
+            self.selectBlockWindow = SelectNewBlockTypeWindow(self, lambda a=idx : self.selectBlockTypeWindowCloseCallback(a))
+
+    def selectBlockTypeWindowCloseCallback(self, idx: int):
+        if self.selectBlockWindow != None:
+            if self.selectBlockWindow.value != None:
+                self.addBlockSelected(self.selectBlockWindow.value, idx)
+            self.selectBlockWindow = None
+
+    def addBlockSelected(self, type: str, idx: int):
+        print(type, idx)
+
+class ScriptBlock(customtkinter.CTkFrame):
+    def __init__(self, master, blocksList: list, key: int, deleteCommand = None, **kwargs):
+        super().__init__(master, corner_radius=0, **kwargs)
+        self.columnconfigure(1, weight=1)
+        self.blocksList = blocksList
+        self.key = key
+        self.blockData = blockData = blocksList[key]
+        self.deleteCommand = deleteCommand
+
+        self.type = type = self.blockData["type"]
+
+        blockLabel = customtkinter.CTkLabel(self, width=100, text=type, fg_color="transparent", anchor="w")
+        blockLabel.grid(column=0, row=0, sticky="w", padx=10)
+
+        blockDeleteBtn = customtkinter.CTkButton(self, text="x", command=self.deleteBlock, width=20, height=10, fg_color="transparent", corner_radius=0)
+        blockDeleteBtn.grid(column=2, row=0)
+        ToolTip(blockDeleteBtn, "Delete block")
+
+        if type == "downloadFile":
+            self.stringVar1 = customtkinter.StringVar(self, blockData["file"])
+            self.stringVar2 = customtkinter.StringVar(self, blockData["output"])
+            self.newStringEntry("file", self.stringVar1, 1)
+            self.newStringEntry("output", self.stringVar2, 2)
+        elif type == "downloadRelease":
+            self.stringVar1 = customtkinter.StringVar(self, blockData["repo"])
+            self.stringVar2 = customtkinter.StringVar(self, blockData["file"])
+            self.stringVar3 = customtkinter.StringVar(self, blockData["output"])
+            self.booleanVar1 = customtkinter.BooleanVar(self, blockData["includePrereleases"])
+            self.newStringEntry("repo", self.stringVar1, 1)
+            self.newStringEntry("file", self.stringVar2, 2)
+            self.newStringEntry("output", self.stringVar3, 3)
+            self.newBooleanCheckbox("includePrereleases", self.booleanVar1, 4)
+        elif type == "extractFile":
+            self.stringVar1 = customtkinter.StringVar(self, blockData["file"])
+            self.stringVar2 = customtkinter.StringVar(self, blockData["input"])
+            self.stringVar3 = customtkinter.StringVar(self, blockData["output"])
+            self.newStringEntry("file", self.stringVar1, 1)
+            self.newStringEntry("input", self.stringVar2, 2)
+            self.newStringEntry("output", self.stringVar3, 3)
+        elif type == "installCia":
+            self.stringVar1 = customtkinter.StringVar(self, blockData["file"])
+            self.newStringEntry("file", self.stringVar1, 1)
+        elif type == "bootTitle":
+            self.stringVar1 = customtkinter.StringVar(self, blockData["TitleID"])
+            self.stringVar2 = customtkinter.StringVar(self, blockData["NAND"])
+            self.newStringEntry("TitleID", self.stringVar1, 1)
+            self.newStringEntry("NAND", self.stringVar2, 2)
+        elif type == "mkdir":
+            self.stringVar1 = customtkinter.StringVar(self, blockData["directory"])
+            self.newStringEntry("directory", self.stringVar1, 1)
+        elif type == "rmdir":
+            self.stringVar1 = customtkinter.StringVar(self, blockData["directory"])
+            self.booleanVar1 = customtkinter.BooleanVar(self, blockData["required"])
+            self.newStringEntry("directory", self.stringVar1, 1)
+            self.newBooleanCheckbox("required", self.booleanVar1, 2)
+        elif type == "move":
+            self.stringVar1 = customtkinter.StringVar(self, blockData["old"])
+            self.stringVar2 = customtkinter.StringVar(self, blockData["new"])
+            self.newStringEntry("old", self.stringVar1, 1)
+            self.newStringEntry("new", self.stringVar2, 2)
+        elif type == "copy":
+            self.stringVar1 = customtkinter.StringVar(self, blockData["source"])
+            self.stringVar2 = customtkinter.StringVar(self, blockData["destination"])
+            self.newStringEntry("source", self.stringVar1, 1)
+            self.newStringEntry("destination", self.stringVar2, 2)
+        elif type == "deleteFile":
+            self.stringVar1 = customtkinter.StringVar(self, blockData["file"])
+            self.newStringEntry("file", self.stringVar1, 1)
+        elif type == "promptMessage":
+            self.stringVar1 = customtkinter.StringVar(self, blockData["message"])
+            self.stringVar2 = customtkinter.StringVar(self, blockData["count"])
+            self.newStringEntry("message", self.stringVar1, 1)
+            self.newStringEntry("count", self.stringVar2, 2)
+        elif type == "skip":
+            self.stringVar1 = customtkinter.StringVar(self, blockData["count"])
+            self.newStringEntry("count", self.stringVar1, 1)
+        elif type == "exit":
+            pass
+    
+    def newStringEntry(self, name, var, r):
+        self.newLabel(name, 0, r)
+        self.newEntry(var, 1, r)
+
+    def newBooleanCheckbox(self, name, var, r):
+        self.newLabel(name, 0, r)
+        self.newCheckbox(var, 1, r)
+
+    def newLabel(self, name, c: int, r: int):
+        lbl = customtkinter.CTkLabel(self, text=name, width=50, compound="left", anchor="w")
+        lbl.grid(column=c, row=r, padx=10, pady=(0, 5), sticky="w")
+
+    def newEntry(self, var, c: int, r: int):
+        entry = customtkinter.CTkEntry(self, textvariable=var, border_width=0, corner_radius=0)
+        entry.grid(column=c, row=r, sticky="we", padx=(0, 5), pady=(0, 5), columnspan=2)
+
+    def newCheckbox(self, var, c: int, r: int):
+        cb = customtkinter.CTkCheckBox(self, text="", variable=var, border_width=1)
+        cb.grid(column=c, row=r, sticky="w", pady=(0, 5))
+
+    def deleteBlock(self):
+        self.blocksList.pop(self.key)
+        if self.deleteCommand != None:
+            self.deleteCommand()
 
 class ScriptEditorWindow(tkinter.Toplevel):
     def __init__(self, master, storeData: StoreContent, key: int):
         super().__init__(master)
-        self.geometry("450x400")
-        self.resizable(False, False)
+        self.geometry("600x400")
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
 
@@ -109,8 +377,21 @@ class ScriptEditorWindow(tkinter.Toplevel):
         for name, value in storeData.storeContent[key].items():
             if name != "info":
                 scriptView = ScriptViewFrame(self.mainframe, storeData.storeContent[key], name)
-                scriptView.grid(column=0, row=i, sticky="we")
+                scriptView.grid(column=0, row=i, sticky="we", padx=10, pady=(5, 0))
                 i += 1
+        
+        self.bindid = self.bind("<Button-1>", self.on_click, add="+")
+        self.protocol("WM_DELETE_WINDOW", self.on_close)
+
+    def on_close(self):
+        self.unbind("<Button-1>", self.bindid)
+        self.destroy()
+
+    def on_click(self, event):
+        widget = event.widget
+        #print(type(widget))
+        if not isinstance(widget, tkinter.Entry):
+            self.focus_set()
 
 class EditEntryWindow(tkinter.Toplevel):
     def __init__(self, master, storeData: StoreContent, key: int):
@@ -195,8 +476,8 @@ class EditEntryWindow(tkinter.Toplevel):
 
 class StoreElementButton(customtkinter.CTkFrame):
     def __init__(self, master: StoreElementsFrame, content: StoreContent, elementIdx: int, **kwargs):
-        self.master = master
         super().__init__(master, bg_color="transparent", corner_radius=0, **kwargs)
+        self.master = master
         btnTitle = content.storeContent[elementIdx]["info"]["title"]
         maxLen = 30 if len(btnTitle) > 30 else len(btnTitle)
         btnImage = Image.open(ICONS_DIR.joinpath(SPRITESHEET_CONTENT[content.storeContent[elementIdx]["info"]["icon_index"]]))
@@ -225,16 +506,16 @@ class StoreElementButton(customtkinter.CTkFrame):
         self.btn.configure(fg_color="transparent")
         self.label.configure(fg_color="transparent")
 
-    def openEditWindow(self, event):
+    def openEditWindow(self, event=None):
         self.btn.configure(fg_color="#1b4a80")
         self.label.configure(fg_color="#3B6594")
-        EditEntryWindow(self, self.content, self.elementIdx).focus()
+        EditEntryWindow(None, self.content, self.elementIdx).focus()
         pass
 
 class StoreElementsFrame(customtkinter.CTkScrollableFrame):
     def __init__(self, master: App, content: StoreContent, **kwargs):
-        self.master = master
         super().__init__(master, **kwargs)
+        self.master = master
         self.content = content
         self.columnconfigure((0, 1, 2, 3), weight=1)
 
